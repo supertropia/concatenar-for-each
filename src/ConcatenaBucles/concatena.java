4c5197ea1880ba963e5cfe2fbea31570317a2277
Lorena Sclar <supertropia@gmail.com>
2023-02-22 17:46:45 -0300

    ejercico de concatenar palabras con diclo for each

---
+++ src/ConcatenaBucles/concatena.java
@@ -0,0 +1,14 @@
+package ConcatenaBucles;
+
+public class concatena {
+    public static void main(String[] args) {
+
+        String[] palabras = {"esta", "es", "una", "frase", "concatenada"};
+
+        for (String words : palabras) {
+
+            System.out.print(" ");
+            System.out.print(words);
+        }
+    }
+}
